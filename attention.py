e1201bf25719ab7f45db52b7cce4550df352ab40
rossoneriqzx <138179677+rossoneriqzx@users.noreply.github.com>
2024-08-07 19:17:52 -0400

    Update attention.py

--- attention.py
+++ attention.py
@@ -4,7 +4,7 @@ from torch import nn
 # Embedding dimensions
 num_hiddens = 256
 
-def attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, M: torch.Tensor):
+def attention(Q, K, V, M): # all are tensor
     A = Q @ K.transpose(-1, -2) / (Q.shape[-1] ** 0.5)
     M = M.unsqueeze(1)
     A.masked_fill_(M == 0, -torch.tensor(float('inf')))
@@ -12,45 +12,45 @@ def attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, M: torch.Tensor
     O = A @ V
     return O
 
-def transpose_qkv(QKV: torch.Tensor):
-    QKV = QKV.reshape(QKV.shape[0], QKV.shape[1], 4, QKV.shape[-1] // 4)
-    QKV = QKV.transpose(-2, -3)
-    return QKV
+def transpose_1(A):
+    A = A.reshape(A.shape[0], A.shape[1], 4, A.shape[-1] // 4)
+    A = A.transpose(-2, -3)
+    return A
 
-def transpose_o(O: torch.Tensor):
+def transpose_2(O):
     O = O.transpose(-2, -3)
     O = O.reshape(O.shape[0], O.shape[1], -1)
     return O
 
 # Multi-head attention
 class Attention_block(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super(Attention_block, self).__init__(*args, **kwargs)
-        self.Wq = nn.Linear(num_hiddens, num_hiddens, bias=False)
-        self.Wk = nn.Linear(num_hiddens, num_hiddens, bias=False)
-        self.Wv = nn.Linear(num_hiddens, num_hiddens, bias=False)
-        self.Wo = nn.Linear(num_hiddens, num_hiddens, bias=False)
+    def __init__(self):
+        super(Attention_block, self).__init__()
+        self.Wq = nn.Linear(num_hiddens, num_hiddens, bias=True)
+        self.Wk = nn.Linear(num_hiddens, num_hiddens, bias=True)
+        self.Wv = nn.Linear(num_hiddens, num_hiddens, bias=True)
+        self.Wo = nn.Linear(num_hiddens, num_hiddens, bias=True) # Bias may be False based on 
     
     def forward(self, X, M: torch.Tensor):
         Q, K, V = self.Wq(X), self.Wk(X), self.Wv(X)
-        Q, K, V = transpose_qkv(Q), transpose_qkv(K), transpose_qkv(V)
+        Q, K, V = transpose_1(Q), transpose_1(K), transpose_1(V)
         O = attention(Q, K, V, M)
-        O = transpose_o(O)
+        O = transpose_2(O)
         O = self.Wo(O)
         return O
 # Cross attention
 class CrossAttention_block(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super(CrossAttention_block, self).__init__(*args, **kwargs)
-        self.Wq = nn.Linear(num_hiddens, num_hiddens, bias=False)
-        self.Wk = nn.Linear(num_hiddens, num_hiddens, bias=False)
-        self.Wv = nn.Linear(num_hiddens, num_hiddens, bias=False)
-        self.Wo = nn.Linear(num_hiddens, num_hiddens, bias=False)
+    def __init__(self):
+        super(CrossAttention_block, self).__init__()
+        self.Wq = nn.Linear(num_hiddens, num_hiddens, bias=True)
+        self.Wk = nn.Linear(num_hiddens, num_hiddens, bias=True)
+        self.Wv = nn.Linear(num_hiddens, num_hiddens, bias=True)
+        self.Wo = nn.Linear(num_hiddens, num_hiddens, bias=True)
     
     def forward(self, X, X_en, I_M):
         Q, K, V = self.Wq(X), self.Wk(X_en), self.Wv(X_en)
-        Q, K, V = transpose_qkv(Q), transpose_qkv(K), transpose_qkv(V)
+        Q, K, V = transpose_1(Q), transpose_1(K), transpose_1(V)
         O = attention(Q, K, V, I_M)
-        O = transpose_o(O)
+        O = transpose_2(O)
         O = self.Wo(O)
         return O
